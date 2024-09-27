9485c4138e00ab45571be41269f71a07f7ed976f
Erica Bournival <erica.bournival@dawsoncollege.qc.ca>
2024-09-27 09:15:33 -0400

    add functions for labs and quizzes

--- grade_calculator.py
+++ grade_calculator.py
@@ -8,5 +8,15 @@ mid1 = float(input("Enter grade for Midterm 1: "))
 mid2 = float(input("Enter grade for Midterm 2: "))
 final = float(input("Enter grade for Final Exam: "))
 prep = float(input("Enter grade for Midterms and Final Preparation: "))
-mark = (((lab)/6)*20) + (((quiz)/6)*15) + ((((ass1)+(ass2)+(ass3)+(ass4))/4)*0.16) + ((((mid1)+(mid2))/2)*0.25) + ((final)*0.18) + ((prep)*0.06)
+def labs(labs_done: int) -> float:
+    if labs_done >6:
+        return 1
+    else:
+        return (lab/6)
+def quizzes(quizzes_done: int) -> float:
+    if quizzes_done > 6:
+        return 1
+    else:
+        return (quiz/6)
+mark = (labs(lab)*20) + (quizzes(quiz)*15) + ((((ass1)+(ass2)+(ass3)+(ass4))/4)*0.16) + ((((mid1)+(mid2))/2)*0.25) + ((final)*0.18) + ((prep)*0.06)
 print("Your grade is: ", round(mark,2))
